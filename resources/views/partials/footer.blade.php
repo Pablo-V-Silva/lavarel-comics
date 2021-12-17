69ecd3e94c8b5284f3cfb081179cbb3f4c6dc686
Pablo-V-Silva <booleanpablosilva@gmail.com>
2021-12-17 22:18:04 +0100

    finish footer and fixed header

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,2 +1,87 @@
-<footer id="siteFooter"></footer>
+<footer id="siteFooter">
+  <div class="contacts_jumbo">
+    <div class="container h-100 ">
+      <div class="row row-cols-2 h-100">
+        <div class="col-6 pt-3">
+          <div class="row">
+            <div class="col-4">
+              <h3 class="text-uppercase">dc-comics</h3>
+              <ul class="list-unstyled">
+                <li>lorem</li>
+                <li>ipsum</li>
+                <li>doloren</li>
+                <li>lorem</li>
+                <li>ipsum</li>
+                <li>doloren</li>
+                <li>doloren</li>
+
+              </ul>
+
+              <h3 class="text-uppercase">shop</h3>
+              <ul class="list-unstyled">
+                <li>lorem</li>
+                <li>ipsum</li>
+                <li>doloren</li>
+              </ul>
+            </div>
+            <div class="col-4">
+              <h3 class="text-uppercase">dc</h3>
+              <ul class="list-unstyled">
+              <li>lorem</li>
+              <li>ipsum</li>
+              <li>doloren</li>
+              <li>lorem</li>
+              <li>ipsum</li>
+              <li>doloren</li><li>lorem</li>
+              <li>ipsum</li>
+              <li>doloren</li><li>lorem</li>
+              <li>ipsum</li>
+              <li>doloren</li>
+              </ul>
+            </div>
+            <div class="col-4">
+              <h3 class="text-uppercase">sites</h3>
+              <ul class="list-unstyled">
+                <li>lorem</li>
+                <li>ipsum</li>
+                <li>doloren</li>
+                <li>lorem</li>
+                <li>ipsum</li>
+                <li>doloren</li>
+              </ul>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. <span class="text-primary">Nobis cum illo</span> libero aliquid consectetur suscipit quod assumenda autem odio amet.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-6 dcLogoFooter"></div>
+      </div>
+    </div>
+  </div>
+
+  <div class="credits">
+    <div class="container">
+      <div class="row row-cols-2">
+        <div class="col d-flex align-items-center">
+          <button class="text-uppercase py-2 px-3 myBtn">sing up now</button>
+        </div>
+        <div class="col">
+          <div class="socials d-flex justify-content-end align-items-center py-3">
+            <span class="text-uppercase fw-bold text-primary pe-3">follow us</span>
+            <div class="socialIcons">
+              <img class="pe-2" src="img/footer-facebook.png" alt="">
+              <img class="pe-2" src="img/footer-twitter.png" alt="">
+              <img class="pe-2" src="img/footer-youtube.png" alt="">
+              <img class="pe-2" src="img/footer-pinterest.png" alt="">
+              <img src="img/footer-periscope.png" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
     
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,32 +1,32 @@
 <header id="siteHeader ">
   <div class="blueBanner">
-    <ul class="list-unstyled d-flex justify-content-end align-items-center container text-light py-3">
-      <li class="text-uppercase pe-3">dc power®visa®</li>
-      <li class="text-uppercase">additional id sites <span><i class="fas fa-caret-down"></i></span></li>
-    </ul>
+    <div class="list-unstyled d-flex justify-content-end align-items-center container text-light py-3">
+      <div class="text-uppercase pe-3">dc power®visa®</div>
+      <div class="text-uppercase">additional id sites <span><i class="fas fa-caret-down"></i></span></div>
+    </div>
   </div>
 
       <div class="navbarContain container d-flex align-items-center">
         <img src="img/dc-logo.png" alt="" height="100px">
         <nav class="w-100 d-flex justify-content-end align-items-center">
-          <ul class="list-unstyled d-flex justify-content-end pt-3">
-            <li class="text-uppercase pe-3">caracthers</li>
-            <li class="text-uppercase pe-3">comics</li>
-            <li class="text-uppercase pe-3">movies</li>
-            <li class="text-uppercase pe-3">tv</li>
-            <li class="text-uppercase pe-3">games</li>
-            <li class="text-uppercase pe-3">collectionables</li>
-            <li class="text-uppercase pe-3">videos</li>
-            <li class="text-uppercase pe-3">fans</li>
-            <li class="text-uppercase pe-3">news</li>
-            <li class="text-uppercase pe-3">shop</li>
+          <ul class="list-unstyled d-flex justify-content-end">
+            <li class="text-uppercase pe-3"><a>caracthers</a></li>
+            <li class="text-uppercase pe-3"><a>comics</a></li>
+            <li class="text-uppercase pe-3"><a>movies</a></li>
+            <li class="text-uppercase pe-3"><a>tv</a></li>
+            <li class="text-uppercase pe-3"><a>games</a></li>
+            <li class="text-uppercase pe-3"><a>collectionables</a></li>
+            <li class="text-uppercase pe-3"><a>videos</a></li>
+            <li class="text-uppercase pe-3"><a>fans</a></li>
+            <li class="text-uppercase pe-3"><a>news</a></li>
+            <li class="text-uppercase pe-3"><a>shop</a></li>
           </ul>
 
 
-          <form action="/" class="searchbar_container">
-            <div class="">
-              <input type="text" name="search" id="searchHeader" placeholder="Search" class="text-end border-0 ">
-              <button><i class="fas fa-search"></i></button>
+          <form action="/" class="">
+            <div class="w-75 d-flex searchbar_container">
+              <input type="text" name="search" id="searchHeader" placeholder="Search" class="text-end border-0 w-100">
+              <button class="searchBtn"><i class="fas fa-search"></i></button>
             </div>
           </form>
         </nav>
